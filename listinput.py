5bf34519c48131998b712db6656beff3dd2a39a7
devi14-asw <devisrii1415@gmail.com>
2025-02-26 15:50:44 +0530

    Add files via upload

---
+++ listinput.py
@@ -0,0 +1,9 @@
+#getting int and float in horizontal line
+'''l=list(map(float,input().split()))
+print(l)'''
+
+'''l=list(map(int,input().split()))
+print(l)'''
+
+l=list(map(float,input().split()))[:5]
+print(l)
