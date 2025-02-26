5b19422cc25933213fb238c5aadc8cbfcf4bdea9
devi14-asw <devisrii1415@gmail.com>
2025-02-26 15:44:12 +0530

    Add files via upload

---
+++ pass or fail.py
@@ -0,0 +1,9 @@
+n=int(input("Enter your mark:"))
+if n>30 and n<70:
+    print("Pass")
+    print("Need more improvement")
+elif n>70:
+    print("Pass")
+    print("Well and good")
+else:
+    print("fail")
