46d54838b28ad78794a3f14a73733969dad02762
devi14-asw <devisrii1415@gmail.com>
2025-02-26 15:59:55 +0530

    Add files via upload

---
+++ userdefined.py
@@ -0,0 +1,26 @@
+#userdefined function
+'''x=input("Enter a number:")
+y=input("Enter a number:")
+def add(a,b):
+    c=a+b
+    return c
+print(add(x,y))
+print("10"+"20")'''
+
+def add():
+    a=10
+    b=20
+    c=a+b
+    print(c)
+def sub():
+    a=10
+    b=20
+    c=a-b
+    print(c)
+add()
+sub()
+
+#empty function
+def add():
+    pass
+print("pass ")
